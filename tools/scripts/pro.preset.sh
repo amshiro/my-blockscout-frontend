28cbdb59e8e7ad8368ab32b2f7b05d2e8815b4ab
amshiro <30610179+amshiro@users.noreply.github.com>
2024-05-24 10:52:40 +0800

    Create pro.preset.sh

---
+++ tools/scripts/pro.preset.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# if [ "$#" -ne 1 ]; then
+#   echo "Usage: yarn pro:preset <preset_name>"
+#   exit 1
+# fi
+
+# preset_name="$1"
+# config_file="./configs/envs/.env.${preset_name}"
+config_file="./configs/envs/.env.base"
+secrets_file="./configs/envs/.env.secrets"
+
+if [ ! -f "$config_file" ]; then
+    echo "Error: File '$config_file' not found."
+    exit 1
+fi
+
+# download assets for the running instance
+dotenv \
+  -e $config_file \
+  -- bash -c './deploy/scripts/download_assets.sh ./public/assets'
+
+yarn svg:build-sprite
+echo ""
+
+# generate envs.js file and run the app
+ # -v NEXT_PUBLIC_GIT_COMMIT_SHA=$(git rev-parse --short HEAD) \
+  # -v NEXT_PUBLIC_GIT_TAG=$(git describe --tags --abbrev=0) \
+dotenv \
+  -v NEXT_PUBLIC_GIT_COMMIT_SHA=aaaaaaa \
+  -v NEXT_PUBLIC_GIT_TAG=v0.0.1 \
+  -e $config_file \
+  -e $secrets_file \
+  -- bash -c './deploy/scripts/make_envs_script.sh && next build' |
+pino-pretty
